8a93e56de979df4ff94e8300dcca417fc459886d
codehumane <codehumane@gmail.com>
2017-06-11 19:02:34 +0900

    modular exponentiation algorithm improvement by power of two

--- src/main/java/math/ModularExponentiation.java
+++ src/main/java/math/ModularExponentiation.java
@@ -3,7 +3,7 @@ package math;
 /**
  * 모듈러 연산
  */
-public class ModularExponentiation {
+class ModularExponentiation {
 
     /**
      * 주어진 밑(base)과 지수(exponentiation)에 대한 모듈러(N) 연산의 결과를 반환함.
@@ -13,10 +13,46 @@ public class ModularExponentiation {
      * @param N    모듈러
      * @return 모듈러 연산 결과
      */
-    public static int calc(int base, int expo, int N) {
+    static int calc(int base, int expo, int N) {
         if (expo == 0)
             return 1;
 
         return (calc(base, expo - 1, N) * base) % N;
+    }
+
+    /**
+     * 제곱을 이용한 모듈러 지수 연산
+     */
+    static int calcBySquare(int base, int expo, int N) {
+        if (expo == 1)
+            return base % N;
+
+        final int sqr = onlyBySqr(base, expo / 2, N);
+        final int rest = calc(base, expo % 2, N);
+        return (sqr * sqr * rest) % N;
+    }
+
+    private static int onlyBySqr(int base, int expo, int N) {
+        if (expo == 1)
+            return base % N;
+
+        final int sqrBase = onlyBySqr(base, expo / 2, N);
+        return (sqrBase * sqrBase) % N;
+    }
+
+    /**
+     * 2의 지수를 활용한 모듈러 지수 연산
+     */
+    static int calcByPowerOf2(int base, int expo, int N) {
+        int sum = 1;
+        int tmp = 1;
+
+        while (expo >= tmp) {
+            if ((tmp & expo) == tmp)
+                sum *= onlyBySqr(base, tmp, N);
+            tmp = tmp << 1;
+        }
+
+        return sum % N;
     }
 }
--- src/test/java/math/ModularExponentiationTest.java
+++ src/test/java/math/ModularExponentiationTest.java
@@ -24,4 +24,45 @@ public class ModularExponentiationTest {
         assertEquals(5, ModularExponentiation.calc(3, 5, 7));
     }
 
+    @Test
+    public void calcBySquare_밑이_5이고_모듈러_N이_3인_모듈러_지수_연산() throws Exception {
+        assertEquals(2, ModularExponentiation.calcBySquare(5, 1, 3));
+        assertEquals(1, ModularExponentiation.calcBySquare(5, 2, 3));
+        assertEquals(2, ModularExponentiation.calcBySquare(5, 3, 3));
+        assertEquals(1, ModularExponentiation.calcBySquare(5, 4, 3));
+        assertEquals(2, ModularExponentiation.calcBySquare(5, 5, 3));
+    }
+
+    @Test
+    public void calcBySquare_밑이_3이고_모듈러_N이_7인_모듈러_지수_연산() throws Exception {
+        assertEquals(3, ModularExponentiation.calcBySquare(3, 1, 7));
+        assertEquals(2, ModularExponentiation.calcBySquare(3, 2, 7));
+        assertEquals(6, ModularExponentiation.calcBySquare(3, 3, 7));
+        assertEquals(4, ModularExponentiation.calcBySquare(3, 4, 7));
+        assertEquals(5, ModularExponentiation.calcBySquare(3, 5, 7));
+    }
+
+    @Test
+    public void calcByPowerOf2_밑이_5이고_모듈러_N이_3인_모듈러_지수_연산() throws Exception {
+        assertEquals(2, ModularExponentiation.calcByPowerOf2(5, 1, 3));
+        assertEquals(1, ModularExponentiation.calcByPowerOf2(5, 2, 3));
+        assertEquals(2, ModularExponentiation.calcByPowerOf2(5, 3, 3));
+        assertEquals(1, ModularExponentiation.calcByPowerOf2(5, 4, 3));
+        assertEquals(2, ModularExponentiation.calcByPowerOf2(5, 5, 3));
+    }
+
+    @Test
+    public void calcByPowerOf2_밑이_3이고_모듈러_N이_7인_모듈러_지수_연산() throws Exception {
+        assertEquals(3, ModularExponentiation.calcByPowerOf2(3, 1, 7));
+        assertEquals(2, ModularExponentiation.calcByPowerOf2(3, 2, 7));
+        assertEquals(6, ModularExponentiation.calcByPowerOf2(3, 3, 7));
+        assertEquals(4, ModularExponentiation.calcByPowerOf2(3, 4, 7));
+        assertEquals(5, ModularExponentiation.calcByPowerOf2(3, 5, 7));
+    }
+
+    public void calc_밑이_5이고_지수가_117이면서_모듈러_N이_19인_경우() throws Exception {
+        assertEquals(1, ModularExponentiation.calc(5, 117, 19));
+        assertEquals(1, ModularExponentiation.calcBySquare(5, 117, 19));
+        assertEquals(1, ModularExponentiation.calcByPowerOf2(5, 117, 19));
+    }
 }
