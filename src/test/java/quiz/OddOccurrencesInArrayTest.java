235411d90e3436ce90398078bd201227c44e868e
codehumane <codehumane@gmail.com>
2018-02-04 18:38:22 +0900

    OddOccurencesInArray add.
    - https://app.codility.com/programmers/lessons/2-arrays/odd_occurrences_in_array

---
+++ src/test/java/quiz/OddOccurrencesInArrayTest.java
@@ -0,0 +1,25 @@
+package quiz;
+
+import lombok.val;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OddOccurrencesInArrayTest {
+
+    @Test
+    public void solution() throws Exception {
+        val odd = new OddOccurrencesInArray();
+        assertEquals(1, odd.solution(new int[]{1}));
+        assertEquals(3, odd.solution(new int[]{1, 3, 1}));
+        assertEquals(1, odd.solution(new int[]{1, 7, 7, 7, 7}));
+        assertEquals(7, odd.solution(new int[]{1, 7, 3, 1, 3}));
+        assertEquals(7, odd.solution(new int[]{1, 7, 3, 1, 3, 5, 9, 9, 5, 1, 3, 1, 3}));
+
+        // 사실 짝수여도 상관 없다. 트릭인 듯.
+        assertEquals(2, odd.solution(new int[]{1, 2, 1}));
+        assertEquals(8, odd.solution(new int[]{1, 7, 8, 7, 1}));
+    }
+
+}
