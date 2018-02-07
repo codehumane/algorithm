7c891cee4ffe35651588e3ac392e08ec0a4fa371
codehumane <codehumane@gmail.com>
2018-02-07 23:40:05 +0900

    `PermMissingElement` add

---
+++ src/main/java/quiz/PermMissingElement.java
@@ -0,0 +1,21 @@
+package quiz;
+
+/**
+ * https://app.codility.com/programmers/lessons/3-time_complexity/perm_missing_elem/
+ */
+class PermMissingElement {
+
+    int solution(int[] A) {
+
+        int N = A.length;
+        int sumOfA = 0;
+        int sum1ToN1 = N + 1;
+
+        for (int i = 0; i < N; i++) {
+            sumOfA += A[i];
+            sum1ToN1 += i + 1;
+        }
+
+        return sum1ToN1 - sumOfA;
+    }
+}
---
+++ src/test/java/quiz/PermMissingElementTest.java
@@ -0,0 +1,21 @@
+package quiz;
+
+import lombok.val;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PermMissingElementTest {
+
+    @Test
+    public void solution() throws Exception {
+        val el = new PermMissingElement();
+
+        assertEquals(4, el.solution(new int[]{2, 3, 1, 5}));
+        assertEquals(1, el.solution(new int[]{2, 3, 4, 5}));
+        assertEquals(1, el.solution(new int[]{2}));
+        assertEquals(2, el.solution(new int[]{1}));
+        assertEquals(1, el.solution(new int[]{}));
+    }
+
+}
