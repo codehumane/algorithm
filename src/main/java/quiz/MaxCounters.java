e25f85ea3259ec5eccb18ac58b144101cd9a7de6
codehumane <codehumane@gmail.com>
2018-02-09 22:33:34 +0900

    `MaxCounters` edit for time complexity O(N+M)

--- src/main/java/quiz/MaxCounters.java
+++ src/main/java/quiz/MaxCounters.java
@@ -1,45 +1,58 @@
 package quiz;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
+ * https://app.codility.com/programmers/lessons/4-counting_elements/max_counters/
+ * <p>
  * Complexity:
  * <p>
  * expected worst-case time complexity is O(N+M);
  * expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
  */
+@Slf4j
 class MaxCounters {
 
     int[] solution(int N, int[] A) {
 
         int max = 0;
+        int maxToApply = 0;
         int M = A.length;
         int[] counters = new int[N];
 
         for (int K = 0; K < M; K++) {
             if (1 <= A[K] && A[K] <= N) {
-                max = increase(A, K, counters, max);
+                max = increase(A, K, counters, max, maxToApply);
             }
 
             if (A[K] == N + 1) {
-                maxCounter(counters, max);
+                maxToApply = max;
             }
+        }
+
+        if (maxToApply > 0) {
+            maxCounter(counters, maxToApply);
         }
 
         return counters;
     }
 
-    private int increase(int[] A, int X, int[] counters, int max) {
+    private int increase(int[] A, int X, int[] counters, int max, int maxToApply) {
         int at = A[X] - 1;
-        counters[at] += 1;
-        if (counters[at] > max) {
-            max = counters[at];
+        if (counters[at] < maxToApply) {
+            counters[at] = maxToApply;
         }
 
-        return max;
+        counters[at] += 1;
+        if (counters[at] > max) return counters[at];
+        else return max;
     }
 
     private void maxCounter(int A[], int max) {
         for (int i = 0; i < A.length; i++) {
-            A[i] = max;
+            if (A[i] < max) {
+                A[i] = max;
+            }
         }
     }
 }
