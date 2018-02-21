5f5650f8487793e63387751465ad825a1c9ad9e1
codehumane <codehumane@gmail.com>
2018-02-21 23:27:57 +0900

    `Distinct` O(N*log(N)) implementation add

--- src/main/java/quiz/Distinct.java
+++ src/main/java/quiz/Distinct.java
@@ -3,25 +3,62 @@ package quiz;
 class Distinct {
 
     int solution(int[] A) {
-
-        int count = 0;
-        int[] distinct = new int[A.length];
-
-        for (int a : A) {
-            if (!contains(distinct, count, a))
-                distinct[count++] = a;
-        }
-
-        return count;
+        mergeSort(A);
+        return countDistinctOnSorted(A);
     }
 
-    private boolean contains(int[] ints, int size, int target) {
-        for (int i = 0; i < size; i++) {
-            if (ints[i] == target) {
-                return true;
+    int[] mergeSort(int[] A) {
+        mergeSort(0, A.length - 1, A);
+        return A;
+    }
+
+    private void mergeSort(int from, int to, int[] A) {
+        if (to - from + 1 < 2) return;
+
+        // 2(0-1)개 : 0,0,1
+        // 3(0-2)개 : 0,1,2
+        // 4(0-3)개 : 0,1,3
+        // 5(0-4)개 : 0,2,4
+        int mid = (from + to) / 2;
+
+        mergeSort(from, mid, A);
+        mergeSort(mid + 1, to, A);
+
+        merge(A, from, mid, to);
+    }
+
+    private void merge(int[] A, int from, int mid, int to) {
+
+        int leftIdx = from;
+        int rightIdx = mid + 1;
+        int size = to - from + 1;
+        int[] sorted = new int[size];
+        int sortedIdx = 0;
+
+        while (sortedIdx < size) {
+            if (rightIdx > to || (leftIdx <= mid && A[leftIdx] < A[rightIdx])) {
+                sorted[sortedIdx++] = A[leftIdx++];
+            } else {
+                sorted[sortedIdx++] = A[rightIdx++];
             }
         }
 
-        return false;
+        for (int s : sorted) {
+            A[from++] = s;
+        }
+    }
+
+    private int countDistinctOnSorted(int[] A) {
+
+        int last = 0;
+        int count = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (i == 0 || A[i] > last) {
+                last = A[i];
+                count++;
+            }
+        }
+
+        return count;
     }
 }
--- src/test/java/quiz/DistinctSpec.groovy
+++ src/test/java/quiz/DistinctSpec.groovy
@@ -24,4 +24,20 @@ class DistinctSpec extends Specification {
         [2, 1, 1, 2, 3, 1]  | 3
         [-1000000, 1000000] | 2
     }
+
+    def "mergeSort"(int[] input, int[] result) {
+
+        given:
+        def distinct = new Distinct()
+
+        expect:
+        distinct.mergeSort(input) == result
+
+        where:
+        input                    | result
+        [3]                      | [3]
+        [3, 2, 1]                | [1, 2, 3]
+        [1, 2, 3]                | [1, 2, 3]
+        [3, 1, 5, 8, 7, 6, 2, 4] | [1, 2, 3, 4, 5, 6, 7, 8]
+    }
 }
