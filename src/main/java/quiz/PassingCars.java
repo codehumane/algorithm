5b0163248de01b833658d3fca9b30fac3855de24
codehumane <codehumane@gmail.com>
2018-03-04 20:56:13 +0900

    `PassingCars` O(N):O(1) implementation add

---
+++ src/main/java/quiz/PassingCars.java
@@ -0,0 +1,21 @@
+package quiz;
+
+class PassingCars {
+
+    private static final int TO_EAST = 0;
+    private static final int MAX_PASSING = 1000000000;
+
+    int solution(int[] A) {
+
+        int toEast = 0;
+        int passing = 0;
+
+        for (int direction : A) {
+            if (direction == TO_EAST) toEast++;
+            else if (toEast > 0) passing += toEast;
+            if (passing > MAX_PASSING) return -1;
+        }
+
+        return passing;
+    }
+}
---
+++ src/test/java/quiz/PassingCarsSpec.groovy
@@ -0,0 +1,25 @@
+package quiz
+
+import spock.lang.Specification
+
+class PassingCarsSpec extends Specification {
+
+    def "Solution"(int[] A, int count) {
+
+        given:
+        def cars = new PassingCars()
+
+        expect:
+        cars.solution(A) == count
+
+        where:
+        A               | count
+        [0, 1, 0, 1, 1] | 5
+        [0]             | 0
+        [1]             | 0
+        [0, 1]          | 1
+        [1, 0]          | 0
+        [0, 0]          | 0
+        [1, 1]          | 0
+    }
+}
