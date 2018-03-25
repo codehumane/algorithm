3220d9c7c77dcd657185f9bfeaca858371183f85
codehumane <codehumane@gmail.com>
2018-03-25 20:33:29 +0900

    `MinPerimeterRectangle` O(sqrt(N)) time complexity & O(1) space complexity implementation add

---
+++ src/main/java/quiz/MinPerimeterRectangle.java
@@ -0,0 +1,16 @@
+package quiz;
+
+class MinPerimeterRectangle {
+
+    int solution(int N) {
+        int min = Integer.MAX_VALUE;
+        final double root = Math.sqrt(N);
+        for (int i = 1; i <= root; i++) {
+            if (N % i == 0)
+                min = Math.min((i + N / i) * 2, min);
+        }
+
+        return min;
+    }
+
+}
