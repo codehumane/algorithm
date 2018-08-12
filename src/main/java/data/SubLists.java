f8c48e340f49a12247e243e4db08b63fe254cf7b
codehumane <codehumane@gmail.com>
2018-08-12 17:53:08 +0900

    SubLists(AbstractList as return type) implementation add

--- src/main/java/data/SubLists.java
+++ src/main/java/data/SubLists.java
@@ -1,19 +1,65 @@
 package data;
 
-import java.util.Collections;
+import java.util.AbstractList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.IntPredicate;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
+
+import static java.util.Collections.emptyList;
 
 /**
  * Effective Java, 3rd Edition의 Item 47에서 발췌
  */
 public class SubLists {
 
-    public static <E> Stream<List<E>> of(List<E> list) {
-        return Stream.concat(
-                Stream.of(Collections.emptyList()),
-                prefixes(list).flatMap(SubLists::suffixes));
+    /**
+     * 책에서는 코드가 나오지 않았지만, `AbstractList`를 반환하는 버전은 이렇게 구현되지 않았을까 추정하고 만들어 봄. <br/>
+     * Stream 반환 버전과 다르게, O(n^2)가 되지 않게 함. O(n)으로 개선.
+     */
+    static <E> List<List<E>> listOf(List<E> source) {
+
+        return new AbstractList<List<E>>() {
+
+            @Override
+            public List<E> get(int index) {
+                if (index >= this.size()) throw new IllegalArgumentException();
+                if (index == 0) return emptyList();
+
+                return IntStream
+                        .rangeClosed(1, source.size())
+                        .map(size -> size - 1)
+                        .filter(prefixIndexInRangeFilter(index))
+                        .boxed()
+                        .findFirst()
+                        .map(makeSubList(index))
+                        .orElseThrow(IllegalArgumentException::new);
+            }
+
+            private IntPredicate prefixIndexInRangeFilter(int index) {
+                return sourceIndex -> index >= countSubListSize(sourceIndex) &&
+                        index < countSubListSize(sourceIndex + 1);
+            }
+
+            private Function<Integer, List<E>> makeSubList(int index) {
+                return sourceIndex -> source
+                        .subList(index - countSubListSize(sourceIndex), sourceIndex + 1);
+            }
+
+            @Override
+            public int size() {
+                return countSubListSize(source.size());
+            }
+
+            private int countSubListSize(int n) {
+                return (n * (n + 1)) / 2 + 1;
+            }
+        };
+    }
+
+    static <E> Stream<List<E>> streamOf(List<E> list) {
+        return Stream.concat(Stream.of(emptyList()), prefixes(list).flatMap(SubLists::suffixes));
     }
 
     private static <E> Stream<List<E>> prefixes(List<E> list) {
--- src/test/java/data/SubListsTest.java
+++ src/test/java/data/SubListsTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 
 import static java.util.Arrays.asList;
-import static java.util.Collections.singletonList;
+import static java.util.Collections.emptyList;
 import static java.util.stream.Collectors.toSet;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -14,19 +14,40 @@ import static org.junit.Assert.assertTrue;
 public class SubListsTest {
 
     @Test
-    public void of() throws Exception {
+    public void streamOf() throws Exception {
 
         // when
-        val subLists = SubLists.of(asList("1", "2", "3")).collect(toSet());
+        val subLists = SubLists.streamOf(asList("1", "2", "3")).collect(toSet());
 
         // then
         assertEquals(7, subLists.size());
         assertTrue(subLists.contains(new ArrayList<String>()));
-        assertTrue(subLists.contains(singletonList("1")));
+        assertTrue(subLists.contains(asList("1")));
         assertTrue(subLists.contains(asList("1", "2")));
-        assertTrue(subLists.contains(singletonList("2")));
+        assertTrue(subLists.contains(asList("2")));
         assertTrue(subLists.contains(asList("1", "2", "3")));
         assertTrue(subLists.contains(asList("2", "3")));
-        assertTrue(subLists.contains(singletonList("3")));
+        assertTrue(subLists.contains(asList("3")));
+    }
+
+    @Test
+    public void listOf() throws Exception {
+
+        // when
+        val subLists = SubLists.listOf(asList("1", "2", "3", "4"));
+
+        // then
+        assertEquals(11, subLists.size());
+        assertEquals(emptyList(), subLists.get(0));
+        assertEquals(asList("1"), subLists.get(1));
+        assertEquals(asList("1", "2"), subLists.get(2));
+        assertEquals(asList("2"), subLists.get(3));
+        assertEquals(asList("1", "2", "3"), subLists.get(4));
+        assertEquals(asList("2", "3"), subLists.get(5));
+        assertEquals(asList("3"), subLists.get(6));
+        assertEquals(asList("1", "2", "3", "4"), subLists.get(7));
+        assertEquals(asList("2", "3", "4"), subLists.get(8));
+        assertEquals(asList("3", "4"), subLists.get(9));
+        assertEquals(asList("4"), subLists.get(10));
     }
 }
