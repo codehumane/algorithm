5855dc961512cbe85a6a21ef79bf3ab969e23a0b
codehumane <codehumane@gmail.com>
2019-09-15 22:34:13 +0900

    [HackerRank] Frequency Queries 성능 개선 버전

--- src/main/kotlin/hackerrank/dictionaryandhashmap/FrequencyQueries.kt
+++ src/main/kotlin/hackerrank/dictionaryandhashmap/FrequencyQueries.kt
@@ -1,21 +1,37 @@
 package hackerrank.dictionaryandhashmap
 
 fun freqQuery(queries: Array<Array<Int>>): Array<Int> {
-    val result = Array<Int>(queries.size) { -1 }
     val freq = mutableMapOf<Int, Int>()
+    val presence = mutableMapOf<Int, Int>()
+    val result = Array(queries.size) { -1 }
+    var resultIndex = 0
 
     queries.forEach { query ->
         val operation = query[0]
         val data = query[1]
 
-        freq.putIfAbsent(data, 0)
-
         when (operation) {
-            1 -> freq.computeIfPresent(data) { _, v -> v + 1 }
-            2 -> freq.computeIfPresent(data) { _, v -> if (v > 0) v - 1 else v }
-            3 -> result[result.indexOf(-1)] = if (freq.any { it.value == data }) 1 else 0
+            1 -> {
+                freq.putIfAbsent(data, 0)
+                freq.computeIfPresent(data) { _, v -> v + 1 }
+                val f = freq[data]!!
+                presence.putIfAbsent(f, 0)
+                presence.computeIfPresent(f) { _, v -> v + 1 }
+                presence.computeIfPresent(f - 1) { _, v -> if (v > 1) v - 1 else 0 }
+            }
+            2 -> {
+                val f: Int? = freq[data]
+                if (f != null && f > 0) {
+                    freq.computeIfPresent(data) { _, v -> v - 1 }
+                    presence.computeIfPresent(f) { _, v -> v - 1 }
+                    if (f > 1) {
+                        presence.computeIfPresent(f - 1) { _, v -> v + 1 }
+                    }
+                }
+            }
+            3 -> result[resultIndex++] = if (presence[data] ?: 0 > 0) 1 else 0
         }
     }
 
-    return result.copyOfRange(0, result.indexOf(-1))
+    return result.copyOfRange(0, resultIndex)
 }
