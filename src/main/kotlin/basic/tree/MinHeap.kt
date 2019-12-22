8f28c9c724eda75c77bcc90d955178aeb1410cc3
codehumane <codehumane@gmail.com>
2019-12-22 14:07:03 +0900

    최소 힙(heap) extract 구현

--- src/main/kotlin/basic/tree/MinHeap.kt
+++ src/main/kotlin/basic/tree/MinHeap.kt
@@ -28,15 +28,49 @@ class MinHeap(maxSize: Int) {
         // 일단, 마지막 단계의 가장 오른쪽에 삽입
         array[++lastIndex] = element
         // 자신 보다 큰 부모가 있다면 계속 자리 교환
-        swapWithGreaterParents(element, lastIndex)
+        swapWithBiggerParents(element, lastIndex)
     }
 
-    private fun swapWithGreaterParents(element: Int, elementIndex: Int) {
+    private fun swapWithBiggerParents(element: Int, elementIndex: Int) {
         val parentIndex = parentIndexOf(elementIndex)
         if (array[parentIndex] == null) return
         if (array[parentIndex]!! <= element) return
         swap(array, parentIndex, elementIndex)
-        swapWithGreaterParents(element, parentIndex)
+        swapWithBiggerParents(element, parentIndex)
+    }
+
+    fun extract(): Int? {
+
+        // 루트에서 제거
+        val extracted = array[ROOT_INDEX] ?: return null
+
+        // 가장 아래 우측 원소를 부모로 위치시킴
+        array[ROOT_INDEX] = null
+        swap(array, ROOT_INDEX, lastIndex)
+        lastIndex--
+
+        // 부모로 위치시킬 원소가 없다면, 루트에서 제거한 값을 그대로 반환
+        if (array[ROOT_INDEX] == null) return extracted
+
+        // 임의로 위치시킨 부모를, 자신 보다 큰 자식을 만나기 전까지 자식과의 위치 교환
+        swapWithSmallerChildren(array[ROOT_INDEX]!!, ROOT_INDEX)
+
+        return extracted
+    }
+
+    private fun swapWithSmallerChildren(element: Int, elementIndex: Int) {
+
+        // 자식 중에 더 작은 값 고르기
+        val left = leftChildOf(elementIndex) ?: return
+        val right = rightChildOf(elementIndex) ?: Int.MAX_VALUE
+        val smallerIndex = if (left < right) leftChildIndexOf(elementIndex)
+        else rightChildIndexOf(elementIndex)
+
+        // 선택된 자식보다 부모가 작으면 자리 교환
+        if (element > array[smallerIndex]!!) {
+            swap(array, elementIndex, smallerIndex)
+            swapWithSmallerChildren(element, smallerIndex)
+        }
     }
 
     private fun swap(array: Array<Int?>, i1: Int, i2: Int) {
@@ -45,8 +79,8 @@ class MinHeap(maxSize: Int) {
         array[i2] = temp
     }
 
-    fun extract(): Int {
-        TODO()
+    fun toArray(): Array<Int?> {
+        return array.copyOfRange(1, lastIndex + 1)
     }
 
 }
--- src/test/kotlin/basic/tree/MinHeapTest.kt
+++ src/test/kotlin/basic/tree/MinHeapTest.kt
@@ -1,7 +1,7 @@
 package basic.tree
 
 import basic.tree.MinHeap.Companion.ROOT_INDEX
-import org.junit.Assert.assertEquals
+import org.junit.Assert.*
 import org.junit.Test
 
 class MinHeapTest {
@@ -39,6 +39,7 @@ class MinHeapTest {
          */
         heap.insert(14)
         assertEquals(14, heap.root())
+        assertArrayEquals(arrayOf(14), heap.toArray())
 
         /**
          *   5
@@ -48,6 +49,7 @@ class MinHeapTest {
         heap.insert(5)
         assertEquals(5, heap.root())
         assertEquals(14, heap.leftChildOf(ROOT_INDEX))
+        assertArrayEquals(arrayOf(5, 14), heap.toArray())
 
         /**
          *   5
@@ -58,6 +60,7 @@ class MinHeapTest {
         assertEquals(5, heap.root())
         assertEquals(14, heap.leftChildOf(ROOT_INDEX))
         assertEquals(15, heap.rightChildOf(ROOT_INDEX))
+        assertArrayEquals(arrayOf(5, 14, 15), heap.toArray())
 
         /**
          *     5
@@ -71,6 +74,7 @@ class MinHeapTest {
         assertEquals(13, heap.leftChildOf(ROOT_INDEX))
         assertEquals(15, heap.rightChildOf(ROOT_INDEX))
         assertEquals(14, heap.leftChildOf(heap.leftChildIndexOf(ROOT_INDEX)))
+        assertArrayEquals(arrayOf(5, 13, 15, 14), heap.toArray())
 
         /**
          *     3
@@ -85,6 +89,7 @@ class MinHeapTest {
         assertEquals(15, heap.rightChildOf(ROOT_INDEX))
         assertEquals(14, heap.leftChildOf(heap.leftChildIndexOf(ROOT_INDEX)))
         assertEquals(13, heap.rightChildOf(heap.leftChildIndexOf(ROOT_INDEX)))
+        assertArrayEquals(arrayOf(3, 5, 15, 14, 13), heap.toArray())
 
         /**
          *      3
@@ -100,6 +105,7 @@ class MinHeapTest {
         assertEquals(14, heap.leftChildOf(heap.leftChildIndexOf(ROOT_INDEX)))
         assertEquals(13, heap.rightChildOf(heap.leftChildIndexOf(ROOT_INDEX)))
         assertEquals(15, heap.leftChildOf(heap.rightChildIndexOf(ROOT_INDEX)))
+        assertArrayEquals(arrayOf(3, 5, 7, 14, 13, 15), heap.toArray())
 
         /**
          *      2
@@ -116,5 +122,110 @@ class MinHeapTest {
         assertEquals(13, heap.rightChildOf(heap.leftChildIndexOf(ROOT_INDEX)))
         assertEquals(15, heap.leftChildOf(heap.rightChildIndexOf(ROOT_INDEX)))
         assertEquals(7, heap.rightChildOf(heap.rightChildIndexOf(ROOT_INDEX)))
+        assertArrayEquals(arrayOf(2, 5, 3, 14, 13, 15, 7), heap.toArray())
+    }
+
+    @Test
+    fun `extract 원소가 1개인 경우`() {
+
+        // given
+        val heap = MinHeap(15)
+        val inserted = 1
+        heap.insert(inserted)
+        assertArrayEquals(arrayOf(1), heap.toArray())
+
+        // when
+        val extracted = heap.extract()
+
+        // then
+        assertEquals(inserted, extracted)
+        assertArrayEquals(arrayOf(), heap.toArray())
+    }
+
+    @Test
+    fun `extract 복잡한 경우`() {
+
+        /**
+         * 준비
+         *
+         *      2
+         *    /   \
+         *   5     3
+         *  / \   / \
+         * 14 13 15  4
+         */
+        val heap = MinHeap(15)
+        heap.insert(14)
+        heap.insert(5)
+        heap.insert(15)
+        heap.insert(13)
+        heap.insert(3)
+        heap.insert(4)
+        heap.insert(2)
+        assertArrayEquals(arrayOf(2, 5, 3, 14, 13, 15, 4), heap.toArray())
+
+        /**
+         *      3
+         *    /   \
+         *   5     4
+         *  / \   /
+         * 14 13 15
+         */
+        assertEquals(2, heap.extract())
+        assertArrayEquals(arrayOf(3, 5, 4, 14, 13, 15), heap.toArray())
+
+        /**
+         *      4
+         *    /   \
+         *   5    15
+         *  / \
+         * 14 13
+         */
+        assertEquals(3, heap.extract())
+        assertArrayEquals(arrayOf(4, 5, 15, 14, 13), heap.toArray())
+
+        /**
+         *      5
+         *    /   \
+         *   13    15
+         *  / \
+         * 14
+         */
+        assertEquals(4, heap.extract())
+        assertArrayEquals(arrayOf(5, 13, 15, 14), heap.toArray())
+
+        /**
+         *      13
+         *    /   \
+         *   14    15
+         */
+        assertEquals(5, heap.extract())
+        assertArrayEquals(arrayOf(13, 14, 15), heap.toArray())
+
+        /**
+         *     14
+         *    /
+         *   15
+         */
+        assertEquals(13, heap.extract())
+        assertArrayEquals(arrayOf(14, 15), heap.toArray())
+
+        /**
+         * 루트에만 15가 남은 heap
+         */
+        assertEquals(14, heap.extract())
+        assertArrayEquals(arrayOf(15), heap.toArray())
+
+        /**
+         * 마지막 값을 추출한 뒤 비어 있는 heap
+         */
+        assertEquals(15, heap.extract())
+        assertArrayEquals(arrayOf(), heap.toArray())
+
+        /**
+         * 추출할 것이 없는 비어 있는 heap
+         */
+        assertNull(heap.extract())
+        assertArrayEquals(arrayOf(), heap.toArray())
     }
 }
