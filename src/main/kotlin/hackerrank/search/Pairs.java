278a88d981ce87de35a0c755f0427b92bf8423dc
codehumane <codehumane@gmail.com>
2020-04-30 21:23:43 +0900

    [hackerrank] "Pairs" 간단 리팩토링

--- src/main/kotlin/hackerrank/search/Pairs.java
+++ src/main/kotlin/hackerrank/search/Pairs.java
@@ -1,9 +1,6 @@
 package hackerrank.search;
 
-import lombok.Value;
-
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -16,46 +13,14 @@ public class Pairs {
                 .boxed()
                 .collect(Collectors.toSet());
 
-        final Set<Pair> pairs = new HashSet<>();
+        final long count = Arrays
+                .stream(arr)
+                .filter(n -> numbers.contains(n - k))
+                .count();
 
-        for (int number : arr) {
-            if (numbers.contains(number + k))
-                pairs.add(new Pair(number, number + k));
-
-            if (numbers.contains(number - k))
-                pairs.add(new Pair(number - k, number));
-        }
-
-        return pairs.size();
-    }
-
-    static class Pair {
-
-        private final Integer o1;
-        private final Integer o2;
-
-        public Pair(Integer o1, Integer o2) {
-            this.o1 = (o1 > o2) ? o2 : o1;
-            this.o2 = (o1 > o2) ? o1 : o2;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-
-            Pair pair = (Pair) o;
-
-            if (!o1.equals(pair.o1)) return false;
-            return o2.equals(pair.o2);
-        }
-
-        @Override
-        public int hashCode() {
-            int result = o1.hashCode();
-            result = 31 * result + o2.hashCode();
-            return result;
-        }
+        return Long
+                .valueOf(count)
+                .intValue();
     }
 
 }
