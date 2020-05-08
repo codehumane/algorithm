effde56c998abdfe1cc47da93aa0c2bbdbb16ca6
luffy.hee <luffy.hee@kakaobank.com>
2020-05-08 09:22:54 +0900

    피보나치 추가 (상향식 동적 프로그래밍 구현)

--- src/main/kotlin/math/Fibonacci.kt
+++ src/main/kotlin/math/Fibonacci.kt
@@ -1,11 +1,27 @@
 package math
 
 fun fibonacci(n: Int): Int {
-    val cache = IntArray(n + 1)
-    return fibonacciMemoization(n, cache)
+    return fibonacciTopDown(n)
 }
 
-fun fibonacciMemoization(n: Int, cache: IntArray): Int {
+fun fibonacciTopDown(n: Int): Int {
+    if (n < 2) return n - 1
+
+    val cache = IntArray(n + 1) { -1 }
+    cache[1] = 0
+    cache[2] = 1
+
+    (3..n).forEach {
+        if (cache[it] == -1)
+            cache[it] = cache[it - 1] + cache[it - 2]
+    }
+
+    return cache[n]
+}
+
+private fun fibonacciBottomUp(n: Int) = fibonacciBottomUp(n, IntArray(n + 1) { -1 })
+
+fun fibonacciBottomUp(n: Int, cache: IntArray): Int {
     if (n == 1) return 0
     if (n == 2) return 1
 
