9cc4c98238257352178a680df908037dc7e8ff29
codehumane <codehumane@gmail.com>
2020-07-05 23:17:40 +0900

    [hackerrank] "Fraudulent Activity notifications" O(Nd) 혹은 O(N)으로의 개선

--- src/main/kotlin/hackerrank/sorting/FraudulentActivityNotifications.kt
+++ src/main/kotlin/hackerrank/sorting/FraudulentActivityNotifications.kt
@@ -1,46 +1,51 @@
 package hackerrank.sorting
 
 fun activityNotifications(expenditure: Array<Int>, d: Int): Int {
-    var count = 0
+    var notification = 0
+    val counts = IntArray(200 + 1)
 
+    (0 until d).forEach {
+        counts[expenditure[it]] += 1
+    }
+
+    var startDay = 0
     (d until expenditure.size).forEach {
-        if (isNotification(expenditure, it, d)) count++
+        val median = median(counts, d)
+        if (expenditure[it] >= median * 2) {
+            notification++
+        }
+
+        counts[expenditure[startDay]] -= 1
+        counts[expenditure[it]] += 1
+        startDay++
     }
 
-    return count
+    return notification
 }
 
-fun isNotification(
-    expenditure: Array<Int>,
-    index: Int,
-    range: Int
-): Boolean {
+fun median(counts: IntArray, days: Int): Float {
+    val isOdd = days.rem(2) == 1
+    var accum = 0
+    var left = -1
 
-    val median = median(
-        expenditure,
-        index - range,
-        index - 1
-    )
-
-    return expenditure[index].toFloat() >= (median * 2f)
-}
-
-private fun median(
-    source: Array<Int>,
-    from: Int,
-    to: Int
-): Float {
-
-    val copy = source
-        .copyOfRange(from, to + 1)
-        .apply { sort() }
-
-    val mid = copy.size.div(2)
-
-    return if (copy.size.rem(2) == 0) {
-        (copy[mid] + copy[mid + 1]).div(2f)
-    } else {
-        copy[mid].toFloat()
+    counts.forEachIndexed { index, count ->
+        accum += count
+        if (isOdd) {
+            if (accum - 1 >= days.div(2))
+                return index.toFloat()
+        } else {
+            if (accum == days.div(2)) {
+                left = index
+            }
+            if (accum > days.div(2)) {
+                return if (left == -1) {
+                    index.toFloat()
+                } else {
+                    (left + index).div(2f)
+                }
+            }
+        }
     }
 
+    throw IllegalStateException("invalid fraudulent detection")
 }
--- src/test/kotlin/hackerrank/sorting/FraudulentActivityNotificationsKtTest.kt
+++ src/test/kotlin/hackerrank/sorting/FraudulentActivityNotificationsKtTest.kt
@@ -9,6 +9,7 @@ class FraudulentActivityNotificationsKtTest {
     fun activityNotifications() {
         assertEquals(2, activityNotifications(arrayOf(2, 3, 4, 2, 3, 6, 8, 4, 5), 5))
         assertEquals(0, activityNotifications(arrayOf(1, 2, 3, 4, 4), 4))
+        assertEquals(1, activityNotifications(arrayOf(4, 2, 3, 6, 5), 3))
     }
 
 }
