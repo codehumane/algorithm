3fd54a69e8a6469b6c0c7a05eb815417a0e7eaeb
codehumane <codehumane@gmail.com>
2020-09-28 14:35:55 +0900

    [LeetCode] Odd Even Linked List

---
+++ src/main/java/linkedlist/LinkedListOddEven.java
@@ -0,0 +1,38 @@
+package linkedlist;
+
+/**
+ * Given a singly linked list,
+ * group all odd nodes together followed by the even nodes.
+ * Please note here we are talking about the node number and not the value in the nodes.
+ * <p>
+ * You should try to do it in place.
+ * The program should run in O(1) space complexity and O(nodes) time complexity.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: 1->2->3->4->5->NULL
+ * Output: 1->3->5->2->4->NULL
+ * Example 2:
+ * <p>
+ * Input: 2->1->3->5->6->4->7->NULL
+ */
+public class LinkedListOddEven {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) return null;
+
+        final ListNode oddHead = head;
+        final ListNode evenHead = head.next;
+        ListNode odd = oddHead;
+        ListNode even = evenHead;
+
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            even.next = even.next.next;
+            odd = odd.next;
+            even = even.next;
+        }
+
+        odd.next = evenHead;
+        return head;
+    }
+}
