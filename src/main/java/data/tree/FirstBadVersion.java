309e149a43ff78af7c11dbb8818d82725c4d4830
codehumane <codehumane@gmail.com>
2020-12-05 19:24:06 +0900

    [LeetCode] First Bad Version - Time Limit Exceeded 문제 해결 (원인: overflow)
    - n은 최대 2^31-1까지 될 수 있음.
    - 이는 signed int 형의 최대 값.
    - 검사 범위가 n~n인 경우 기존 로직은 `(n + n) / 2`를 하므로 overflow.
    - `left + (right - left) / 2`로 변경하면 주어진 제약 조건 하에서는 문제 안 됨.

--- src/main/java/data/tree/FirstBadVersion.java
+++ src/main/java/data/tree/FirstBadVersion.java
@@ -14,7 +14,7 @@ public class FirstBadVersion extends VersionControl {
         int right = n;
 
         while (left <= right) {
-            final int mid = (left + right) / 2;
+            final int mid = left + (right - left) / 2;
             if (isBadVersion(mid) && !isBadVersion(mid - 1)) return mid;
             if (isBadVersion(mid) && isBadVersion(mid - 1)) right = mid - 1;
             else left = mid + 1;
