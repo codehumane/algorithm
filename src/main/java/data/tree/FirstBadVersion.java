2629449c8e62eea1de1e97b7d774df63394228a3
codehumane <codehumane@gmail.com>
2020-12-05 19:25:01 +0900

    [LeetCode] First Bad Version - `Binary Search Template II` 방식으로 구현

--- src/main/java/data/tree/FirstBadVersion.java
+++ src/main/java/data/tree/FirstBadVersion.java
@@ -2,6 +2,12 @@ package data.tree;
 
 /**
  * 1 <= bad <= n <= 2^31 - 1
+ * <p>
+ * OOOOX
+ * OOOXX
+ * OOXXX
+ * OXXXX
+ * XXXXX
  */
 public class FirstBadVersion extends VersionControl {
 
@@ -13,14 +19,13 @@ public class FirstBadVersion extends VersionControl {
         int left = 1;
         int right = n;
 
-        while (left <= right) {
+        while (left < right) {
             final int mid = left + (right - left) / 2;
-            if (isBadVersion(mid) && !isBadVersion(mid - 1)) return mid;
-            if (isBadVersion(mid) && isBadVersion(mid - 1)) right = mid - 1;
+            if (isBadVersion(mid)) right = mid; // `mid`가 첫 번째 bad version 일 수도 아닐 수도 있으므로 mid 포함하여 다시 검사
             else left = mid + 1;
         }
 
-        throw new IllegalStateException();
+        return left;
     }
 
 }
