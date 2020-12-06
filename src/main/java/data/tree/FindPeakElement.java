3a134a47f645009530e3176b5f1f717f36b89e4b
codehumane <codehumane@gmail.com>
2020-12-06 09:49:24 +0900

    [LeetCode] Find Peak Element (logN 방식인데 좀 더 간소화)
    - Solution에 소개된 구현.
    - https://leetcode.com/problems/find-peak-element/solution/

--- src/main/java/data/tree/FindPeakElement.java
+++ src/main/java/data/tree/FindPeakElement.java
@@ -2,13 +2,10 @@ package data.tree;
 
 public class FindPeakElement {
     public int findPeakElement(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            final boolean greaterThanLeft = (i - 1 < 0) || nums[i - 1] < nums[i];
-            final boolean greaterThanRight = (i + 1 >= nums.length) || nums[i] > nums[i + 1];
-            if (greaterThanLeft && greaterThanRight) return i;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1]) return i;
         }
-
-        throw new IllegalStateException();
+        return nums.length - 1;
     }
 
 }
