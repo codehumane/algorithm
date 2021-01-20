a035a440aba874537a415d7dfd833e66b6f263e7
codehumane <codehumane@gmail.com>
2021-01-21 08:51:46 +0900

    binary tree inorder traversal - iterative 구현 추가

--- src/main/java/data/tree/BinaryTreeInOrderTraversal.java
+++ src/main/java/data/tree/BinaryTreeInOrderTraversal.java
@@ -1,8 +1,10 @@
 package data.tree;
 
-import java.util.ArrayList;
-import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.*;
+
+@Slf4j
 public class BinaryTreeInOrderTraversal {
 
     /**
@@ -18,6 +20,45 @@ public class BinaryTreeInOrderTraversal {
     // result:
     // 4 2 5 1 3
     public List<Integer> inOrderTraversal(TreeNode root) {
+        final List<Integer> recursive = recursiveApproach(root);
+        final List<Integer> iterative = iterativeApproach(root);
+
+        if (!recursive.equals(iterative)) {
+            throw new IllegalStateException();
+        }
+
+        return iterative;
+    }
+
+    private List<Integer> iterativeApproach(TreeNode root) {
+        final List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+
+        final Set<TreeNode> visited = new HashSet<>();
+        final Deque<TreeNode> deque = new ArrayDeque<>();
+        deque.push(root);
+
+        while (!deque.isEmpty()) {
+            final TreeNode node = deque.peek();
+
+            if (!visited.contains(node)) {
+                visited.add(node);
+                if (node.left != null) {
+                    deque.push(node.left);
+                }
+            } else {
+                deque.pop();
+                result.add(node.val);
+                if (node.right != null) {
+                    deque.push(node.right);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private List<Integer> recursiveApproach(TreeNode root) {
         final List<Integer> result = new ArrayList<>();
         traversal(root, result);
         return result;
--- src/test/java/data/tree/BinaryTreeInOrderTraversalTest.java
+++ src/test/java/data/tree/BinaryTreeInOrderTraversalTest.java
@@ -3,6 +3,8 @@ package data.tree;
 import lombok.val;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.assertEquals;
 
 public class BinaryTreeInOrderTraversalTest {
@@ -13,6 +15,11 @@ public class BinaryTreeInOrderTraversalTest {
     public void inOrderTraversalExample1() {
 
         // given
+        //   1
+        //    \
+        //     2
+        //    /
+        //   3
         val node1 = new TreeNode(1);
         val node2 = new TreeNode(2);
         val node3 = new TreeNode(3);
@@ -43,6 +50,7 @@ public class BinaryTreeInOrderTraversalTest {
     public void inOrderTraversalExample3() {
 
         // given
+        //   1
         val node1 = new TreeNode(1);
 
         // when
@@ -74,6 +82,9 @@ public class BinaryTreeInOrderTraversalTest {
     public void inOrderTraversalExample5() {
 
         // given
+        //  1
+        //   \
+        //    2
         val node1 = new TreeNode(1);
         val node2 = new TreeNode(2);
         node1.right = node2;
@@ -85,6 +96,80 @@ public class BinaryTreeInOrderTraversalTest {
         assertEquals(2, traversed.size());
         assertEquals(node1.val, traversed.get(0).intValue());
         assertEquals(node2.val, traversed.get(1).intValue());
+    }
+
+    @Test
+    public void inOrderCustom1() {
+
+        // given
+        //      1
+        //    /   \
+        //   2     3
+        //  / \   /
+        // 4   5 7
+        //    /   \
+        //   6     8
+        //
+        // result:
+        val node1 = new TreeNode(1);
+        val node2 = new TreeNode(2);
+        val node3 = new TreeNode(3);
+        val node4 = new TreeNode(4);
+        val node5 = new TreeNode(5);
+        val node6 = new TreeNode(6);
+        val node7 = new TreeNode(7);
+        val node8 = new TreeNode(8);
+        node1.left = node2;
+        node1.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+        node5.left = node6;
+        node3.left = node7;
+        node7.right = node8;
+
+        // when
+        val traversed = traversal.inOrderTraversal(node1);
+
+        // then
+        val expected = Arrays.asList(4, 2, 6, 5, 1, 7, 8, 3);
+        assertEquals(expected, traversed);
+    }
+
+    @Test
+    public void inOrderCustom2_같은_값이_2개_이상_존재할_때() {
+
+        // given
+        //      1
+        //    /   \
+        //   2     2
+        //  / \   /
+        // 4   5 7
+        //    /   \
+        //   6     8
+        //
+        // result:
+        val node1 = new TreeNode(1);
+        val node2 = new TreeNode(2);
+        val node2_2 = new TreeNode(2);
+        val node4 = new TreeNode(4);
+        val node5 = new TreeNode(5);
+        val node6 = new TreeNode(6);
+        val node7 = new TreeNode(7);
+        val node8 = new TreeNode(8);
+        node1.left = node2;
+        node1.right = node2_2;
+        node2.left = node4;
+        node2.right = node5;
+        node5.left = node6;
+        node2_2.left = node7;
+        node7.right = node8;
+
+        // when
+        val traversed = traversal.inOrderTraversal(node1);
+
+        // then
+        val expected = Arrays.asList(4, 2, 6, 5, 1, 7, 8, 2);
+        assertEquals(expected, traversed);
     }
 
 }
