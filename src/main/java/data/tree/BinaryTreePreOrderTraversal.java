1370b9776f8f906cf1f800a55676abc01f8ea88a
codehumane <codehumane@gmail.com>
2021-01-24 17:48:20 +0900

    binary tree preorder traversal - iterative 구현 추가

--- src/main/java/data/tree/BinaryTreePreOrderTraversal.java
+++ src/main/java/data/tree/BinaryTreePreOrderTraversal.java
@@ -1,7 +1,6 @@
 package data.tree;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class BinaryTreePreOrderTraversal {
 
@@ -9,15 +8,44 @@ public class BinaryTreePreOrderTraversal {
      * Root -> Left -> Right
      */
     // given:
-    //     1
-    //    / \
-    //   2   3
-    //  / \
-    // 4   5
+    //      1
+    //    /   \
+    //   2     3
+    //  / \   /
+    // 4   5 6
     //
     // result:
-    // 1 2 4 5 3
+    // 1 2 4 5 6 3
     public List<Integer> preOrderTraversal(TreeNode root) {
+        final List<Integer> iterative = byIterative(root);
+        final List<Integer> recursive = byRecursive(root);
+
+        if (!iterative.equals(recursive)) {
+            throw new IllegalStateException();
+        }
+
+        return recursive;
+    }
+
+    private List<Integer> byIterative(TreeNode root) {
+        if (root == null) return Collections.emptyList();
+
+        final List<Integer> result = new ArrayList<>();
+        final Deque<TreeNode> deque = new ArrayDeque<>();
+        deque.push(root);
+
+        while (!deque.isEmpty()) {
+            final TreeNode node = deque.pop();
+            result.add(node.val);
+
+            if (node.right != null) deque.push(node.right);
+            if (node.left != null) deque.push(node.left);
+        }
+
+        return result;
+    }
+
+    private List<Integer> byRecursive(TreeNode root) {
         final List<Integer> result = new ArrayList<>();
         traversal(root, result);
         return result;
