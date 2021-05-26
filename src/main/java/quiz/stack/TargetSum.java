e8661bd844f606a69935398607f4350a0c1fe505
codehumane <codehumane@gmail.com>
2021-05-26 22:46:45 +0900

    [LeetCode] Target Sum (recursive brute force)
    - https://leetcode.com/problems/target-sum/

---
+++ src/main/java/quiz/stack/TargetSum.java
@@ -0,0 +1,52 @@
+package quiz.stack;
+
+/**
+ * You are given an integer array nums and an integer target.<br/>
+ * You want to build an expression out of nums
+ * by adding one of the symbols '+' and '-' before each integer in nums
+ * and then concatenate all the integers.<br/>
+ * <br/>
+ * For example,
+ * if nums = [2, 1],
+ * you can add a '+' before 2 and a '-' before 1
+ * and concatenate them to build the expression "+2-1".<br/>
+ * <br/>
+ * Return the number of different expressions that you can build, which evaluates to target.<br/>
+ * <br/>
+ * Constraints:<br/>
+ * 1. 1 <= nums.length <= 20<br/>
+ * 2. 0 <= nums[i] <= 1000<br/>
+ * 3. 0 <= sum(nums[i]) <= 1000<br/>
+ * 4. -1000 <= target <= 1000<br/>
+ */
+public class TargetSum {
+
+    public int findTargetSumWays(int[] nums, int target) {
+        return find(nums, target, 0, 0);
+    }
+
+    private int find(int[] nums, int target, int sum, int index) {
+        int count = 0;
+
+        if (index < nums.length - 1) {
+            count += find(
+                    nums,
+                    target,
+                    nums[index] + sum,
+                    index + 1
+            );
+            count += find(
+                    nums,
+                    target,
+                    -nums[index] + sum,
+                    index + 1
+            );
+        } else {
+            if (sum + nums[index] == target) count++;
+            if (sum - nums[index] == target) count++;
+        }
+
+        return count;
+    }
+
+}
---
+++ src/test/java/quiz/stack/TargetSumTest.java
@@ -0,0 +1,57 @@
+package quiz.stack;
+
+import lombok.val;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TargetSumTest {
+
+    private final TargetSum solution = new TargetSum();
+
+    @Test
+    public void custom1() {
+        val nums = new int[]{1, 1};
+        val target = 0;
+        val expected = 2;
+        val count = solution.findTargetSumWays(nums, target);
+        assertEquals(expected, count);
+    }
+
+    @Test
+    public void custom2() {
+        val nums = new int[]{1, 1};
+        val target = 1;
+        val expected = 0;
+        val count = solution.findTargetSumWays(nums, target);
+        assertEquals(expected, count);
+    }
+
+    @Test
+    public void custom3() {
+        val nums = new int[]{2, 1, 1};
+        val target = 2;
+        val expected = 2;
+        val count = solution.findTargetSumWays(nums, target);
+        assertEquals(expected, count);
+    }
+
+    @Test
+    public void example1() {
+        val nums = new int[]{1, 1, 1, 1, 1};
+        val target = 3;
+        val expected = 5;
+        val count = solution.findTargetSumWays(nums, target);
+        assertEquals(expected, count);
+    }
+
+    @Test
+    public void example2() {
+        val nums = new int[]{1};
+        val target = 1;
+        val expected = 1;
+        val count = solution.findTargetSumWays(nums, target);
+        assertEquals(expected, count);
+    }
+
+}
