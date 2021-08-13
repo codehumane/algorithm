660df73d3720ad8a56c47fc2231b2b75ef7d7822
codehumane <codehumane@gmail.com>
2021-08-13 22:50:22 +0900

    [LeetCode] Course Schedule
    https://leetcode.com/problems/course-schedule/

---
+++ src/main/java/quiz/CourseSchedule.java
@@ -0,0 +1,85 @@
+package quiz;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class CourseSchedule {
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        final List<Node> nodes = toNodes(numCourses, prerequisites);
+        final Set<Node> visited = new HashSet<>();
+        final Set<Node> tracking = new HashSet<>();
+
+        return nodes
+                .stream()
+                .noneMatch(node -> detectCycle(node, visited, tracking));
+    }
+
+    private List<Node> toNodes(int numCourses, int[][] prerequisites) {
+
+        final List<Node> nodes = IntStream
+                .range(0, numCourses)
+                .mapToObj(Node::new)
+                .collect(Collectors.toList());
+
+        for (int[] prerequisite : prerequisites) {
+            final Node parent = nodes.get(prerequisite[1]);
+            final Node child = nodes.get(prerequisite[0]);
+            parent.vertices.add(child);
+        }
+
+        return nodes;
+    }
+
+    private boolean detectCycle(Node node, Set<Node> visited, Set<Node> tracking) {
+        if (visited.contains(node)) {
+            return tracking.contains(node);
+        }
+
+        visited.add(node);
+        tracking.add(node);
+
+        for (Node vertex : node.vertices) {
+            if (detectCycle(vertex, visited, tracking)) {
+                return true;
+            }
+        }
+
+        tracking.remove(node);
+        return false;
+    }
+
+    static class Node {
+
+        final int value;
+        final Set<Node> vertices;
+
+        public Node(int value) {
+            this.value = value;
+            this.vertices = new HashSet<>();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            return value == ((Node) o).value;
+        }
+
+        @Override
+        public int hashCode() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "value=" + value +
+                    ", vertices=" + vertices.stream().map(x -> x.value).collect(Collectors.toSet()) +
+                    '}';
+        }
+    }
+}
---
+++ src/test/java/quiz/CourseScheduleTest.java
@@ -0,0 +1,85 @@
+package quiz;
+
+import lombok.val;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class CourseScheduleTest {
+
+    private final CourseSchedule solution = new CourseSchedule();
+
+    @Test
+    public void example1() {
+
+        val numCourses = 2;
+        val prerequisites = new int[][]{
+                new int[]{1, 0}
+        };
+
+        // Explanation: There are a total of 2 courses to take.
+        // To take course 1 you should have finished course 0.
+        // So it is possible.
+        assertTrue(solution.canFinish(numCourses, prerequisites));
+    }
+
+    @Test
+    public void example2() {
+
+        val numCourses = 2;
+        val prerequisites = new int[][]{
+                new int[]{1, 0},
+                new int[]{0, 1}
+        };
+
+        // There are a total of 2 courses to take.
+        // To take course 1 you should have finished course 0,
+        // and to take course 0 you should also have finished course 1.
+        // So it is impossible.
+        assertFalse(solution.canFinish(numCourses, prerequisites));
+    }
+
+    @Test
+    public void custom1() {
+
+        val numCourses = 3;
+        val prerequisites = new int[][]{
+                new int[]{1, 0},
+                new int[]{2, 1},
+                new int[]{0, 2}
+        };
+
+        assertFalse(solution.canFinish(numCourses, prerequisites));
+    }
+
+    @Test
+    public void custom2() {
+
+        val numCourses = 4;
+        val prerequisites = new int[][]{
+                new int[]{1, 0},
+                new int[]{2, 1},
+                new int[]{2, 0},
+                new int[]{3, 2},
+                new int[]{0, 3}
+        };
+
+        assertFalse(solution.canFinish(numCourses, prerequisites));
+    }
+
+    @Test
+    public void custom3() {
+
+        val numCourses = 4;
+        val prerequisites = new int[][]{
+                new int[]{1, 0},
+                new int[]{2, 1},
+                new int[]{2, 0},
+                new int[]{3, 2},
+                new int[]{3, 0}
+        };
+
+        assertTrue(solution.canFinish(numCourses, prerequisites));
+    }
+}
