fd1cb106f8c45a197af22da335eab333ba4537bd
codehumane <codehumane@gmail.com>
2022-02-12 19:48:47 +0900

    [LeetCode] Longest Valid Parentheses - 기존 풀이 별도 클래스로 추출

--- src/main/java/quiz/dynamic/LongestValidParentheses.java
+++ src/main/java/quiz/dynamic/LongestValidParentheses.java
@@ -5,6 +5,10 @@ package quiz.dynamic;
  */
 public class LongestValidParentheses {
 
+    public int longestValidParentheses(String s) {
+        return new Wrong().longestValidParentheses(s);
+    }
+
     /**
      * isLeftConcatenating 케이스를 생각 못했는데,
      * 단편적으로 한 번만 왼쪽 결과를 더하는 것뿐만 아니라,
@@ -13,54 +17,56 @@ public class LongestValidParentheses {
      * 그러려면 공간복잡도가 올라감.
      * 결국 이 방식은 오류!
      */
-    public int longestValidParentheses(String s) {
-        var index = s.indexOf("()");
-        var lastLeft = -1;
-        var lastRight = -1;
-        var max = 0;
+    static class Wrong {
+        public int longestValidParentheses(String s) {
+            var index = s.indexOf("()");
+            var lastLeft = -1;
+            var lastRight = -1;
+            var max = 0;
 
-        while (index >= 0) {
-            var l = index;
-            var r = index + 1;
+            while (index >= 0) {
+                var l = index;
+                var r = index + 1;
 
-            while (true) {
-                if (isRightExpanding(s, r)) {
-                    r += 2;
-                } else if (isSurroundExpanding(s, l, r)) {
-                    l -= 1;
-                    r += 1;
-                } else if (isLeftConcatenating(lastLeft, lastRight, l)) {
-                    l = lastLeft;
-                } else {
-                    var length = r - l + 1;
-                    max = Math.max(max, length);
-                    index = s.indexOf("()", r + 1);
-                    lastLeft = l;
-                    lastRight = r;
-                    break;
+                while (true) {
+                    if (isRightExpanding(s, r)) {
+                        r += 2;
+                    } else if (isSurroundExpanding(s, l, r)) {
+                        l -= 1;
+                        r += 1;
+                    } else if (isLeftConcatenating(lastLeft, lastRight, l)) {
+                        l = lastLeft;
+                    } else {
+                        var length = r - l + 1;
+                        max = Math.max(max, length);
+                        index = s.indexOf("()", r + 1);
+                        lastLeft = l;
+                        lastRight = r;
+                        break;
+                    }
                 }
             }
+
+            return max;
         }
 
-        return max;
-    }
+        private boolean isRightExpanding(String s, int rightIdx) {
+            return rightIdx + 2 < s.length()
+                    && s.charAt(rightIdx + 1) == '('
+                    && s.charAt(rightIdx + 2) == ')';
+        }
 
-    private boolean isRightExpanding(String s, int rightIdx) {
-        return rightIdx + 2 < s.length()
-                && s.charAt(rightIdx + 1) == '('
-                && s.charAt(rightIdx + 2) == ')';
-    }
+        private boolean isSurroundExpanding(String s, int leftIdx, int rightIdx) {
+            return 0 < leftIdx
+                    && rightIdx + 1 < s.length()
+                    && s.charAt(leftIdx - 1) == '('
+                    && s.charAt(rightIdx + 1) == ')';
+        }
 
-    private boolean isSurroundExpanding(String s, int leftIdx, int rightIdx) {
-        return 0 < leftIdx
-                && rightIdx + 1 < s.length()
-                && s.charAt(leftIdx - 1) == '('
-                && s.charAt(rightIdx + 1) == ')';
-    }
-
-    private boolean isLeftConcatenating(int lastLeft, int lastRight, int leftIndex) {
-        return 0 <= lastLeft
-                && lastRight + 1 == leftIndex;
+        private boolean isLeftConcatenating(int lastLeft, int lastRight, int leftIndex) {
+            return 0 <= lastLeft
+                    && lastRight + 1 == leftIndex;
+        }
     }
 
 }
