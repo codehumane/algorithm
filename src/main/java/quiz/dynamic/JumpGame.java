615de923834290962a0a3b77f5bee432e5d783b5
codehumane <codehumane@gmail.com>
2022-02-13 13:47:13 +0900

    [LeetCode] Jump Game - DP 이용한 풀이에서 불필요한 스트림 연산을 제거 (Time Limit Exceeded)

--- src/main/java/quiz/dynamic/JumpGame.java
+++ src/main/java/quiz/dynamic/JumpGame.java
@@ -10,12 +10,6 @@ public class JumpGame {
     public boolean canJump(int[] nums) {
         if (nums.length < 2) return true;
 
-        return Arrays
-                .stream(generateDP(nums))
-                .anyMatch(v -> v[v.length - 1]);
-    }
-
-    private boolean[][] generateDP(int[] nums) {
         final var dp = new boolean[nums.length][nums.length];
 
         for (int from = 0; from < nums.length - 1; from++) {
@@ -23,11 +17,15 @@ public class JumpGame {
 
                 for (int to = from + 1; to < nums.length; to++) {
                     dp[from][to] = (to <= from + nums[from]) || (from > 0 && dp[from - 1][to]);
+
+                    if (to == nums.length - 1 && dp[from][to]) {
+                        return true;
+                    }
                 }
             }
         }
 
-        return dp;
+        return false;
     }
 
 }
