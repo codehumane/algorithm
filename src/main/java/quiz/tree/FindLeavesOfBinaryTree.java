f19e8cacfe84aa0ad2f919f915a901c7a1299604
codehumane <codehumane@gmail.com>
2022-02-28 19:43:58 +0900

    [LeetCode] Find Leaves of Binary Tree - 첫 번째 풀이 별도 클래스 추출

--- src/main/java/quiz/tree/FindLeavesOfBinaryTree.java
+++ src/main/java/quiz/tree/FindLeavesOfBinaryTree.java
@@ -9,48 +9,57 @@ import java.util.stream.Collectors;
 public class FindLeavesOfBinaryTree {
 
     public List<List<Integer>> findLeaves(TreeNode root) {
-        var collected = new ArrayList<List<Integer>>();
-        var removed = new HashSet<TreeNode>();
-
-        while ((root.left != null && !removed.contains(root.left)) ||
-                (root.right != null && !removed.contains(root.right))) {
-
-            var leaves = collect(root, removed);
-            var leafValues = leaves
-                    .stream()
-                    .map(l -> l.val)
-                    .collect(Collectors.toList());
-
-            collected.add(leafValues);
-            removed.addAll(leaves);
-        }
-
-        collected.add(List.of(root.val));
-        return collected;
+        return new FirstApproach().findLeaves(root);
     }
 
-    private List<TreeNode> collect(TreeNode root, Set<TreeNode> removed) {
-        var leaves = new ArrayList<TreeNode>();
-        var deque = new ArrayDeque<TreeNode>();
-        deque.offer(root);
+    /**
+     * O(N^2)
+     */
+    static class FirstApproach {
+        public List<List<Integer>> findLeaves(TreeNode root) {
+            var collected = new ArrayList<List<Integer>>();
+            var removed = new HashSet<TreeNode>();
 
-        while (!deque.isEmpty()) {
-            var node = deque.poll();
-            var isLeftEmpty = node.left == null || removed.contains(node.left);
-            var isRightEmpty = node.right == null || removed.contains(node.right);
+            while ((root.left != null && !removed.contains(root.left)) ||
+                    (root.right != null && !removed.contains(root.right))) {
 
-            if (isLeftEmpty && isRightEmpty) {
-                leaves.add(node);
+                var leaves = collect(root, removed);
+                var leafValues = leaves
+                        .stream()
+                        .map(l -> l.val)
+                        .collect(Collectors.toList());
+
+                collected.add(leafValues);
+                removed.addAll(leaves);
             }
-            if (!isLeftEmpty) {
-                deque.offer(node.left);
-            }
-            if (!isRightEmpty) {
-                deque.offer(node.right);
-            }
+
+            collected.add(List.of(root.val));
+            return collected;
         }
 
-        return leaves;
+        private List<TreeNode> collect(TreeNode root, Set<TreeNode> removed) {
+            var leaves = new ArrayList<TreeNode>();
+            var deque = new ArrayDeque<TreeNode>();
+            deque.offer(root);
+
+            while (!deque.isEmpty()) {
+                var node = deque.poll();
+                var isLeftEmpty = node.left == null || removed.contains(node.left);
+                var isRightEmpty = node.right == null || removed.contains(node.right);
+
+                if (isLeftEmpty && isRightEmpty) {
+                    leaves.add(node);
+                }
+                if (!isLeftEmpty) {
+                    deque.offer(node.left);
+                }
+                if (!isRightEmpty) {
+                    deque.offer(node.right);
+                }
+            }
+
+            return leaves;
+        }
     }
 
     static class TreeNode {
