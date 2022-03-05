13cad939fb56293f07cfc7c8841948a0acf874d8
codehumane <codehumane@gmail.com>
2022-03-05 18:29:14 +0900

    [LeetCode] Logger Rate Limiter - Queue & Set 이용

--- src/main/java/quiz/etc/LoggerRateLimiter.java
+++ src/main/java/quiz/etc/LoggerRateLimiter.java
@@ -1,23 +1,68 @@
 package quiz.etc;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class LoggerRateLimiter {
 
-    private final Map<String, Integer> lastLogs = new HashMap<>();
+    private final MapSolution mapSolution = new MapSolution();
+    private final QueueAndSetSolution queueAndSetSolution = new QueueAndSetSolution();
 
     public LoggerRateLimiter() {
 
     }
 
     public boolean shouldPrintMessage(int timestamp, String message) {
-        if (lastLogs.containsKey(message) && timestamp - lastLogs.get(message) < 10) {
+        var byMap = mapSolution.shouldPrintMessage(timestamp, message);
+        var byQueueAndSet = queueAndSetSolution.shouldPrintMessage(timestamp, message);
+        assert byMap == byQueueAndSet;
+        return byMap;
+    }
+
+    static class QueueAndSetSolution {
+
+        private final Set<String> set = new HashSet<>();
+        private final Deque<TimedMessage> deque = new ArrayDeque<>();
+
+        public boolean shouldPrintMessage(int timestamp, String message) {
+
+            while (!deque.isEmpty()) {
+                if (timestamp - deque.peek().timestamp < 10) break;
+
+                var old = deque.poll();
+                set.remove(old.message);
+            }
+
+            if (!set.contains(message)) {
+                set.add(message);
+                deque.offer(new TimedMessage(timestamp, message));
+                return true;
+            }
+
             return false;
         }
 
-        lastLogs.put(message, timestamp);
-        return true;
+        static class TimedMessage {
+            final int timestamp;
+            final String message;
+
+            TimedMessage(int timestamp, String message) {
+                this.timestamp = timestamp;
+                this.message = message;
+            }
+        }
+    }
+
+    static class MapSolution {
+        private final Map<String, Integer> lastLogs = new HashMap<>();
+
+        public boolean shouldPrintMessage(int timestamp, String message) {
+            if (lastLogs.containsKey(message) && timestamp - lastLogs.get(message) < 10) {
+                return false;
+            }
+
+            lastLogs.put(message, timestamp);
+            return true;
+        }
     }
 
 }
