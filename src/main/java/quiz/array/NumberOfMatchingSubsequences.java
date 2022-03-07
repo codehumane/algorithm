2dd02e06fbe1e59a3e1fb5e00152cf3982e103b0
codehumane <codehumane@gmail.com>
2022-03-07 12:38:58 +0900

    [LeetCode] Number of Matching Subsequences - S를 기준으로 iterate 하는 구현 추가 (Time Limit Exceeded)

--- src/main/java/quiz/array/NumberOfMatchingSubsequences.java
+++ src/main/java/quiz/array/NumberOfMatchingSubsequences.java
@@ -1,6 +1,9 @@
 package quiz.array;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 /**
  * https://leetcode.com/problems/number-of-matching-subsequences/
@@ -8,7 +11,58 @@ import java.util.Arrays;
 public class NumberOfMatchingSubsequences {
 
     public int numMatchingSubseq(String s, String[] words) {
-        return new MatchingWordUniquely().numMatchingSubseq(s, words);
+        var iterateSOnce = new IterateSOnlyOnce().numMatchingSubseq(s, words);
+        var effectiveBruteForce = new MatchingWordUniquely().numMatchingSubseq(s, words);
+
+        assert iterateSOnce == effectiveBruteForce;
+        return iterateSOnce;
+    }
+
+    @Slf4j
+    static class IterateSOnlyOnce {
+        public int numMatchingSubseq(String s, String[] words) {
+
+            var heads = Arrays
+                    .stream(words)
+                    .map(Word::new)
+                    .collect(Collectors.toList());
+
+            for (int i = 0; i < s.toCharArray().length; i++) {
+                for (Word head : heads) {
+                    if (!head.isEnd() && head.header() == s.charAt(i)) {
+                        head.increase();
+                    }
+                }
+            }
+
+            return (int) heads
+                    .stream()
+                    .filter(Word::isEnd)
+                    .count();
+        }
+
+        static class Word {
+
+            final String value;
+            int head;
+
+            Word(String value) {
+                this.value = value;
+                this.head = 0;
+            }
+
+            Character header() {
+                return isEnd() ? null : value.charAt(head);
+            }
+
+            boolean isEnd() {
+                return head == value.length();
+            }
+
+            void increase() {
+                head++;
+            }
+        }
     }
 
     static class MatchingWordUniquely {
