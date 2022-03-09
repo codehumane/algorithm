35bc52581febf23d82af1d5c05e1be5929a06172
codehumane <codehumane@gmail.com>
2022-03-09 20:05:36 +0900

    [LeetCode] Decode String (recursion 풀이 별도 클래스로 추출)

--- src/main/java/quiz/string/DecodeString.java
+++ src/main/java/quiz/string/DecodeString.java
@@ -6,60 +6,66 @@ package quiz.string;
 public class DecodeString {
 
     public String decodeString(String s) {
-        return build(s, 0).string;
+        return new UsingRecursion().decodeString(s);
     }
 
-    private Decoding build(String s, int idx) {
-        var builder = new StringBuilder();
+    static class UsingRecursion {
+        public String decodeString(String s) {
+            return build(s, 0).string;
+        }
 
-        while (idx < s.length()) {
-            if (Character.isAlphabetic(s.charAt(idx))) {
-                builder.append(s.charAt(idx));
-                idx++;
+        private Decoding build(String s, int idx) {
+            var builder = new StringBuilder();
 
-            } else if (Character.isDigit(s.charAt(idx))) {
-                var decoded = decode(s, idx);
-                builder.append(decoded.string);
-                idx = decoded.index + 1;
+            while (idx < s.length()) {
+                if (Character.isAlphabetic(s.charAt(idx))) {
+                    builder.append(s.charAt(idx));
+                    idx++;
 
-            } else {
-                break;
+                } else if (Character.isDigit(s.charAt(idx))) {
+                    var decoded = decode(s, idx);
+                    builder.append(decoded.string);
+                    idx = decoded.index + 1;
+
+                } else {
+                    break;
+                }
             }
+
+            return new Decoding(builder.toString(), idx);
         }
 
-        return new Decoding(builder.toString(), idx);
-    }
+        private Decoding decode(String s, int idx) {
+            var bracketOpenIndex = indexOfBracketOpen(s, idx);
+            var repeat = Integer.parseInt(s.substring(idx, bracketOpenIndex));
+            var decoded = build(s, bracketOpenIndex + 1);
 
-    private Decoding decode(String s, int idx) {
-        var bracketOpenIndex = indexOfBracketOpen(s, idx);
-        var repeat = Integer.parseInt(s.substring(idx, bracketOpenIndex));
-        var decoded = build(s, bracketOpenIndex + 1);
+            var repeated = String
+                    .valueOf(decoded.string)
+                    .repeat(Math.max(0, repeat));
 
-        var repeated = String
-                .valueOf(decoded.string)
-                .repeat(Math.max(0, repeat));
-
-        return new Decoding(repeated, decoded.index);
-    }
-
-    private int indexOfBracketOpen(String s, int idx) {
-        while (Character.isDigit(s.charAt(idx))) {
-            idx++;
+            return new Decoding(repeated, decoded.index);
         }
 
-        return idx;
-    }
+        private int indexOfBracketOpen(String s, int idx) {
+            while (Character.isDigit(s.charAt(idx))) {
+                idx++;
+            }
 
-    static class Decoding {
-
-        final String string;
-        final int index;
-
-        public Decoding(String string, int index) {
-            this.string = string;
-            this.index = index;
+            return idx;
         }
 
+        static class Decoding {
+
+            final String string;
+            final int index;
+
+            public Decoding(String string, int index) {
+                this.string = string;
+                this.index = index;
+            }
+
+        }
     }
 
 }
