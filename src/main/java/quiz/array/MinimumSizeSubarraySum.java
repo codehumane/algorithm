e05870f73ce8632b3fe2e604cfb27b00ee7844b5
codehumane <codehumane@gmail.com>
2022-03-11 11:44:20 +0900

    [LeetCode] Minimum Size Subarray Sum (로직 간소화)

--- src/main/java/quiz/array/MinimumSizeSubarraySum.java
+++ src/main/java/quiz/array/MinimumSizeSubarraySum.java
@@ -3,26 +3,22 @@ package quiz.array;
 public class MinimumSizeSubarraySum {
 
     public int minSubArrayLen(int target, int[] nums) {
+
         var sum = 0;
-        var sumStart = 0;
-        var sumLength = nums.length;
+        var from = 0;
+        var length = Integer.MAX_VALUE;
 
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
-            if (target > sum) continue;
 
-            var simSum = sum;
-            var simStart = sumStart;
-            while (simStart <= i && simSum >= target) {
-                simSum -= nums[simStart++];
+            while (sum >= target) {
+                length = Math.min(length, i - from + 1);
+                sum -= nums[from++];
             }
-
-            sumStart = simStart - 1;
-            sum = simSum + nums[simStart - 1];
-            sumLength = Math.min(sumLength, i - sumStart + 1);
         }
 
-        return sum >= target ? sumLength : 0;
+        // 1 <= nums.length <= 10^5 제약이 있어서 가능
+        return length == Integer.MAX_VALUE ? 0 : length;
     }
 
 }
