8ac033dc0894a7206e8f7ebf4e4db00dd1d12aa5
codehumane <codehumane@gmail.com>
2022-04-14 10:59:01 +0900

    [LeetCode] Bulls and Cows - 기존 구현 별도 클래스로 추출

--- src/main/java/quiz/string/BullsAndCows.java
+++ src/main/java/quiz/string/BullsAndCows.java
@@ -13,57 +13,65 @@ import java.util.stream.Collectors;
 public class BullsAndCows {
 
     public String getHint(String secret, String guess) {
-        var counts = countSecretChars(secret);
-        var hint = toHint(secret, guess, counts);
-        return hint.toString();
+        return new TwoPass().getHint(secret, guess);
     }
 
-    private Map<Character, Long> countSecretChars(String secret) {
+    static class TwoPass {
 
-        return secret
-                .chars()
-                .mapToObj(c -> (char) c)
-                .collect(Collectors.groupingBy(
-                        Function.identity(),
-                        Collectors.counting()
-                ));
-    }
-
-    private Hint toHint(String secret,
-                        String guess,
-                        Map<Character, Long> counts) {
-
-        var hint = new Hint();
-
-        for (int i = 0; i < guess.length(); i++) {
-            var gc = guess.charAt(i);
-            if (counts.containsKey(gc)) {
-
-                if (gc == secret.charAt(i)) {
-                    hint.bulls++;
-
-                    // 앞에서 bulls만 있었던 경우는 counts의 특성상 0보다 작을 수 없고,
-                    // 따라서 0보다 작은 것은 cows가 있었던 경우에 한하며,
-                    // bulls가 우선하므로,
-                    // cows를 감소.
-                    if (counts.get(gc) <= 0) {
-                        hint.cows--;
-                    }
-                } else {
-
-                    // bulls가 우선이므로,
-                    // bulls에 이미 다 카운팅한 경우라면,
-                    // cows 계산은 생략.
-                    if (counts.get(gc) > 0) {
-                        hint.cows++;
-                    }
-                }
-
-                counts.put(gc, counts.get(gc) - 1);
-            }
+        public String getHint(String secret, String guess) {
+            var counts = countSecretChars(secret);
+            var hint = toHint(secret, guess, counts);
+            return hint.toString();
         }
 
-        return hint;
+        private Map<Character, Long> countSecretChars(String secret) {
+
+            return secret
+                    .chars()
+                    .mapToObj(c -> (char) c)
+                    .collect(Collectors.groupingBy(
+                            Function.identity(),
+                            Collectors.counting()
+                    ));
+        }
+
+        private Hint toHint(String secret,
+                            String guess,
+                            Map<Character, Long> counts) {
+
+            var hint = new Hint();
+
+            for (int i = 0; i < guess.length(); i++) {
+                var gc = guess.charAt(i);
+                if (counts.containsKey(gc)) {
+
+                    if (gc == secret.charAt(i)) {
+                        hint.bulls++;
+
+                        // 앞에서 bulls만 있었던 경우는 counts의 특성상 0보다 작을 수 없고,
+                        // 따라서 0보다 작은 것은 cows가 있었던 경우에 한하며,
+                        // bulls가 우선하므로,
+                        // cows를 감소.
+                        if (counts.get(gc) <= 0) {
+                            hint.cows--;
+                        }
+                    } else {
+
+                        // bulls가 우선이므로,
+                        // bulls에 이미 다 카운팅한 경우라면,
+                        // cows 계산은 생략.
+                        if (counts.get(gc) > 0) {
+                            hint.cows++;
+                        }
+                    }
+
+                    counts.put(gc, counts.get(gc) - 1);
+                }
+            }
+
+            return hint;
+        }
+
     }
 
     static class Hint {
