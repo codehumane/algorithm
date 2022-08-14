169daddad89a1773ff3a87d737522d54bb1ba366
codehumane <codehumane@gmail.com>
2022-08-14 11:19:58 +0900

    [LeetCode] 3. Longest Substring Without Repeating Characters > sliding window

---
+++ src/main/java/quiz/string/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,35 @@
+package quiz.string;
+
+import java.util.HashSet;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+
+    public int lengthOfLongestSubstring(String s) {
+        var chars = new HashSet<Character>(s.length());
+        var longest = 0;
+        var left = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            var c = s.charAt(i);
+
+            if (chars.contains(c)) {
+                longest = Math.max(longest, chars.size());
+
+                for (int l = left; l < i; l++) {
+                    var d = s.charAt(l);
+                    chars.remove(d);
+
+                    if (d == c) {
+                        left = l + 1;
+                        break;
+                    }
+                }
+            }
+
+            chars.add(c);
+        }
+
+        return Math.max(longest, chars.size());
+    }
+
+}
---
+++ src/test/java/quiz/string/LongestSubstringWithoutRepeatingCharactersTest.java
@@ -0,0 +1,29 @@
+package quiz.string;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LongestSubstringWithoutRepeatingCharactersTest {
+
+    private final LongestSubstringWithoutRepeatingCharacters solution =
+            new LongestSubstringWithoutRepeatingCharacters();
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            // example
+            "3,abcabcbb",
+            "1,bbbbb",
+            "3,pwwkew",
+            "1,' '", // blank string " "
+            "1,b",
+            "3,aaabc",
+            "3,dvdf"
+    })
+    public void verify(int output, String input) {
+        var actual = solution.lengthOfLongestSubstring(input);
+        assertEquals(output, actual);
+    }
+
+}
