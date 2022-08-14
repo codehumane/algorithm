da8cf3c3382f4a28282f33f37c4006786a165572
codehumane <codehumane@gmail.com>
2022-08-14 17:18:33 +0900

    [LeetCode] 3. Longest Substring Without Repeating Characters > 기존 구현 별도 클래스로 추출

--- src/main/java/quiz/string/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/quiz/string/LongestSubstringWithoutRepeatingCharacters.java
@@ -5,31 +5,37 @@ import java.util.HashSet;
 public class LongestSubstringWithoutRepeatingCharacters {
 
     public int lengthOfLongestSubstring(String s) {
-        var chars = new HashSet<Character>(s.length());
-        var longest = 0;
-        var left = 0;
+        return new MySlidingWindow().lengthOfLongestSubstring(s);
+    }
 
-        for (int i = 0; i < s.length(); i++) {
-            var c = s.charAt(i);
+    static class MySlidingWindow {
+        public int lengthOfLongestSubstring(String s) {
+            var chars = new HashSet<Character>(s.length());
+            var longest = 0;
+            var left = 0;
 
-            if (chars.contains(c)) {
-                longest = Math.max(longest, chars.size());
+            for (int i = 0; i < s.length(); i++) {
+                var c = s.charAt(i);
 
-                for (int l = left; l < i; l++) {
-                    var d = s.charAt(l);
-                    chars.remove(d);
+                if (chars.contains(c)) {
+                    longest = Math.max(longest, chars.size());
 
-                    if (d == c) {
-                        left = l + 1;
-                        break;
+                    for (int l = left; l < i; l++) {
+                        var d = s.charAt(l);
+                        chars.remove(d);
+
+                        if (d == c) {
+                            left = l + 1;
+                            break;
+                        }
                     }
                 }
+
+                chars.add(c);
             }
 
-            chars.add(c);
+            return Math.max(longest, chars.size());
         }
-
-        return Math.max(longest, chars.size());
     }
 
 }
