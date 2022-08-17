1552b615a97bd95edfaf63ef2b4cb5b019dc76ec
codehumane <codehumane@gmail.com>
2022-08-17 09:02:49 +0900

    [LeetCode] 146. LRU Cache > doubly linked list 구현
    https://leetcode.com/problems/lru-cache/

--- src/main/java/quiz/linkedlist/LRUCache.java
+++ src/main/java/quiz/linkedlist/LRUCache.java
@@ -1,7 +1,9 @@
 package quiz.linkedlist;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * https://leetcode.com/problems/lru-cache/
@@ -9,12 +11,14 @@ import java.util.Map;
 public class LRUCache {
 
     private final LinkedHashMapExtension map;
+    private final UsingDoublyLinkedList linkedList;
 
     /**
      * Initialize the LRU cache with positive size capacity
      */
     public LRUCache(int capacity) {
         this.map = new LinkedHashMapExtension(capacity);
+        this.linkedList = new UsingDoublyLinkedList(capacity);
     }
 
     /**
@@ -24,6 +28,7 @@ public class LRUCache {
      * O(1)
      */
     public int get(int key) {
+        assert map.get(key) == linkedList.get(key);
         return map.get(key);
     }
 
@@ -36,6 +41,7 @@ public class LRUCache {
      * O(1)
      */
     public void put(int key, int value) {
+        linkedList.put(key, value);
         map.put(key, value);
     }
 
@@ -60,6 +66,107 @@ public class LRUCache {
         @Override
         protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
             return super.size() > cacheSize;
+        }
+    }
+
+    static class UsingDoublyLinkedList {
+
+        private final Map<Integer, Entry> entries;
+        private final int capacity;
+        private Entry head;
+        private Entry tail;
+
+        public UsingDoublyLinkedList(int capacity) {
+            this.entries = new HashMap<>(capacity);
+            this.capacity = capacity;
+        }
+
+        public int get(int key) {
+            if (!entries.containsKey(key)) return -1;
+
+            var entry = entries.get(key);
+            moveToLast(entry);
+            return entry.value;
+        }
+
+        public void put(int key, int value) {
+            if (entries.containsKey(key)) {
+                var entry = entries.get(key);
+                entry.value = value;
+                moveToLast(entry);
+            } else {
+                var entry = new Entry(key, value);
+                addToLast(key, entry);
+            }
+        }
+
+        private void moveToLast(Entry entry) {
+            // 이미 tail이면 할 것이 X
+            if (tail == entry) return;
+
+            // entry 중간 연결에서 끊어내기
+            if (entry.before != null) entry.before.after = entry.after;
+            if (entry.after != null) entry.after.before = entry.before;
+            if (head == entry) head = entry.after;
+
+            // entry 새로 연결
+            entry.after = null;
+            entry.before = tail;
+            if (tail != null) tail.after = entry;
+            tail = entry;
+        }
+
+        private void addToLast(int key, Entry entry) {
+            entries.put(key, entry);
+
+            if (tail != null) {
+                tail.after = entry;
+                entry.before = tail;
+            }
+            tail = entry;
+
+            if (head == null) {
+                head = entry;
+            } else if (entries.size() > capacity) {
+                entries.remove(head.key);
+                head.after.before = null;
+                head = head.after;
+            }
+        }
+
+        static class Entry {
+
+            final int key;
+
+            int value;
+            Entry before;
+            Entry after;
+
+            Entry(int key, int value) {
+                this.key = key;
+                this.value = value;
+            }
+
+            @Override
+            public boolean equals(Object o) {
+                if (this == o) return true;
+                if (o == null || getClass() != o.getClass()) return false;
+                Entry entry = (Entry) o;
+                return key == entry.key;
+            }
+
+            @Override
+            public int hashCode() {
+                return Objects.hash(key);
+            }
+
+            @Override
+            public String toString() {
+                return "Entry{" +
+                        "key=" + key +
+                        ", value=" + value +
+                        '}';
+            }
         }
     }
 
