551deb96357c06100b570319cf83003462fbbfc7
codehumane <codehumane@gmail.com>
2023-01-01 13:12:26 +0900

    [LeetCode] 3Sum - HashSet 풀이 추출

--- src/main/java/quiz/math/ThreeSum.java
+++ src/main/java/quiz/math/ThreeSum.java
@@ -17,33 +17,38 @@ import java.util.List;
 public class ThreeSum {
 
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-
-        var res = new ArrayList<List<Integer>>();
-        for (int i = 0; i < nums.length && nums[i] <= 0; ++i) {
-            if (i == 0 || nums[i] != nums[i - 1]) {
-                collect(nums, i, res);
-            }
-        }
-
-        return res;
+        return new ByHashSet().threeSum(nums);
     }
 
-    void collect(int[] nums, int i, List<List<Integer>> res) {
-        var complements = new HashSet<Integer>();
+    static class ByHashSet {
+        public List<List<Integer>> threeSum(int[] nums) {
+            Arrays.sort(nums);
 
-        for (int j = i + 1; j < nums.length; ++j) {
-            int complement = -(nums[i] + nums[j]);
-
-            if (complements.contains(complement)) {
-                res.add(List.of(nums[i], nums[j], complement));
-                while (j + 1 < nums.length && nums[j] == nums[j + 1]) {
-                    ++j;
+            var res = new ArrayList<List<Integer>>();
+            for (int i = 0; i < nums.length && nums[i] <= 0; ++i) {
+                if (i == 0 || nums[i] != nums[i - 1]) {
+                    collect(nums, i, res);
                 }
             }
 
-            complements.add(nums[j]);
+            return res;
+        }
+
+        void collect(int[] nums, int i, List<List<Integer>> res) {
+            var complements = new HashSet<Integer>();
+
+            for (int j = i + 1; j < nums.length; ++j) {
+                int complement = -(nums[i] + nums[j]);
+
+                if (complements.contains(complement)) {
+                    res.add(List.of(nums[i], nums[j], complement));
+                    while (j + 1 < nums.length && nums[j] == nums[j + 1]) {
+                        ++j;
+                    }
+                }
+
+                complements.add(nums[j]);
+            }
         }
     }
-
 }
